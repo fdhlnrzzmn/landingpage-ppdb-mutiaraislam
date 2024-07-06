5f24cc4ca353a05ba045caff41e075f72aec9cc9
Fadhil <79124345+fdhlnrzzmn@users.noreply.github.com>
2024-07-06 09:11:52 +0800

    feat: add some section (#1)

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,19 @@ module.exports = {
   content: ['./**/*.{html, js}'],
   theme: {
     extend: {
-      colors : {
-      primaryColor : '#70C2C9',
-      secondaryColor : '#edfeff',
-      thirdColor : '#0f1729',
-      fourthColor : '#FFD73D',
-    },
-      fontFamily :{
-        poppins:['Poppins, sans-serif'],
-        playfair:['Playfair Display, serif'],
-      }
+      colors: {
+        primaryColor: '#70C2C9',
+        secondaryColor: '#edfeff',
+        thirdColor: '#0f1729',
+        fourthColor: '#FFD73D',
+        fifthColor: '#70C4CA',
+        sixthColor: '#001938',
+      },
+      fontFamily: {
+        poppins: ['Poppins, sans-serif'],
+        playfair: ['Playfair Display, serif'],
+      },
     },
   },
   plugins: [],
-}
-
+};
